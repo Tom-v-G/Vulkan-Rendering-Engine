3068a6a1287c547cbd9969697b9ce31c1308a4b8
Tom-v-G <tom.v.gelooven@gmail.com>
2025-08-26 12:07:29 +0200

    First Commit

---
+++ shaders/compile.sh
@@ -0,0 +1,2 @@
+/home/tom/Programs/vulkansdk-linux-x86_64-1.4.309.0/1.4.309.0/x86_64/bin/glslc shaders/shader.vert -o shaders/vert.spv
+/home/tom/Programs/vulkansdk-linux-x86_64-1.4.309.0/1.4.309.0/x86_64/bin/glslc shaders/shader.frag -o shaders/frag.spv
